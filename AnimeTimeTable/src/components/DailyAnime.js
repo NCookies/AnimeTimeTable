d05c2a6ed243083d4e20b5af46e4eb6d826c0d97
NCookies <swstar21c@gmail.com>
2017-04-30 10:39:39 +0900

    네트워크의 연결을 확인하여 DB에서 정보를 가져올지 결정해야 함. 네트워크로 받아올 때 어떤 식으로 값을 저장하고 전달할지 구상해야 함

--- AnimeTimeTable/src/components/DailyAnime.js
+++ AnimeTimeTable/src/components/DailyAnime.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, AsyncStorage } from 'react-native';
+import
+{   View,
+    Text,
+    AsyncStorage,
+    NetInfo }
+    from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
 import SampleInfo from './SampleInfo';
@@ -8,23 +13,61 @@ import Bookmark from './Bookmark'
 
 export default class DailyAnime extends Component {
 
-    async getMoviesFromApi() {
+    async getMoviesFromApi(weekday) {
         let animeData;
 
-        try {
-            let response = await fetch('http://www.anissia.net/anitime/list?w=0');
+        const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+        'Thursday', 'Friday', 'Saturday']
+
+
+        try {   // 요일별로 애니 정보를 가져옴
+            let response = await fetch('http://www.anissia.net/anitime/list?w='
+                                        + weekday);
+
             let animeData = await response.json();
+            await AsyncStorage.setItem('@' + dayOfWeek[weekday] + 'Anime:key', animeData);
+            // DB에 데이터 저장
 
             return animeData;
+
         } catch(error) {
             console.error(error);
         }
     }
 
     async componentDidMount() {
-        var animeData = await this.getMoviesFromApi();
-        console.log(animeData);
 
+        // 네트워크 연결 확인
+        // NetInfo.isConnected.fetch().then(isConnected => {
+        //     console.log('First, is ' + (isConnected ? 'online' : 'offline'));
+        // });
+        // function handleFirstConnectivityChange(isConnected) {
+        //     console.log('Then, is ' + (isConnected ? 'online' : 'offline'));
+        //     NetInfo.isConnected.removeEventListener(
+        //         'change',
+        //         handleFirstConnectivityChange
+        //     );
+        // }
+        // NetInfo.isConnected.addEventListener(
+        //     'change',
+        //     handleFirstConnectivityChange
+        // );
+
+        // 0 ~ 6 : 일요일 ~ 토요일
+        for (let i = 0; i < 7; i++) {
+            await this.getMoviesFromApi(i);
+            // 변수를 어떤 방식으로 저장해야될지 모르겠음
+        }
+
+
+        // try {
+        //     const animeData = await AsyncStorage.getItem('@sundayAnime:key');
+        //     if (animeData !== null) {
+        //         console.log('data : ', animeData);
+        //     }
+        // } catch (error) {
+        //     console.log(error);
+        // }
     }
 
     render() {
